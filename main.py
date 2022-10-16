2611fdf5470678dbb9320115d37fcfc4818e8365
uribo <ur1b0.dev@gmail.com>
2022-10-16 22:57:23 +0900

    jsonから巡回先を見るようにした

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import requests
 from bs4 import BeautifulSoup
+import json
 
 def check_stock_akiduki(url):
     sp = BeautifulSoup(requests.get(url).text, "html.parser")
@@ -8,9 +9,19 @@ def check_stock_akiduki(url):
     isStock = False if sp.find_all(class_="cart_tdc_sbn")[0].find("img") is not None else True
     return title, isStock
 
+# 設定値読み込み
+f = open("./config.json", "r")
+conf = json.loads(f.read())
+f.close()
 
-url = "https://akizukidenshi.com/catalog/g/gM-16834/"
-print(check_stock_akiduki(url))
-
-url = "https://akizukidenshi.com/catalog/g/gM-13641/"
-print(check_stock_akiduki(url))
+msg = ""
+for url in conf["stores"]["item_list"]:
+    # 在庫チェック
+    try:
+        title,stock =  check_stock_akiduki(url)
+        if stock:
+            msg = msg + f"{title}\n{url}\n"
+    except KeyError as e:
+        print(f"failed:Key {e} is not found.")
+    except Exception as e:
+        print(f"failed:{e}")
