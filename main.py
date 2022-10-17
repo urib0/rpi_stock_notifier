fd779566e71da87ba9eec18f8766908266f8cfbf
uribo <ur1b0.dev@gmail.com>
2022-10-17 14:00:48 +0900

    周期実行を追加

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import time
 
 def send_message(token,message):
     headers = {
@@ -17,26 +18,32 @@ def check_stock_akiduki(url):
     sp = BeautifulSoup(requests.get(url).text, "html.parser")
     title = sp.find("title").text.split(":")[0]
     isStock = False if sp.find_all(class_="cart_tdc_sbn")[0].find("img") is not None else True
+    print(title, isStock)
     return title, isStock
 
-# 設定値読み込み
-f = open("./config.json", "r")
-conf = json.loads(f.read())
-f.close()
-print(conf)
+while(True):
+    # 設定値読み込み
+    with open("./config.json", "r") as f:
+        conf = json.loads(f.read())
 
-msg = "\n"
-for store in conf["stores"]:
-    for url in store["item_list"]:
-        # 在庫チェック
-        try:
-            title,stock =  check_stock_akiduki(url)
-            if stock:
-                msg = msg + f"{title}\n{url}\n"
-        except KeyError as e:
-            print(f"failed:Key {e} is not found.")
-        except Exception as e:
-            print(f"failed:{e}")
+    # 在庫チェック&メッセージ作成
+    msg = "\n"
+    for store in conf["stores"]:
+        for url in store["item_list"]:
+            # 在庫チェック
+            try:
+                title,stock =  check_stock_akiduki(url)
+                if stock:
+                    msg = msg + f"{title}\n{url}\n"
+            except KeyError as e:
+                print(f"failed:Key {e} is not found.")
+            except Exception as e:
+                print(f"failed:{e}")
 
-if len(msg) != 0:
-    send_message(conf["line_token"],msg)
+    # LINE送信
+    if len(msg) != 0:
+        ret = send_message(conf["line_token"],msg)
+        print(f"line notifier response:{ret}")
+
+    time.sleep(conf["interval"])
+
